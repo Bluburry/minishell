46b4b79f83376c2af6ea0e12505f667c212f50fe
Jessica Maria Carvalho Carrico <jecarval@c1r8s17.42porto.com>
2023-10-27 14:27:40 +0100

    history

--- src/minishell.c
+++ src/minishell.c
@@ -9,25 +9,19 @@ void	waiting_for_input(t_env *env)
 		rl = readline("minishell > ");
 		printf("rl: %s\n", rl);
 		lexer(rl, env);
+		add_history(rl);
 	}
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	t_env	*env;
+	t_env		*env;
 
 	(void)argc;
 	(void)argv;
 	env = create_env_struct(envp);
-/*	int i = -1;
-	while (++i < env->size)
-	{
-		printf("%s\n", env->vars[i]);
-	} */
-/* 	printf("\n\nSearch: %s\n", get_env_var(env, "LANG"));
-	printf("\n\nSearch: %s\n", get_env_var(env, "PWD"));
-	printf("\n\nSearch: %s\n", get_env_var(env, "LS_COLORS")); */
 	waiting_for_input(env);
 	clear_env_struct(env);
+	rl_clear_history();
 	return (0);
 }
