9a72fc00874ef93074ff5f21f219a9b35e2944a9
Sheepcoat <ambmoura@live.com.pt>
2023-10-31 09:29:09 +0000

    wip

--- src/lexer/var_to_value.c
+++ src/lexer/var_to_value.c
@@ -23,14 +23,20 @@ static char	*store_word(char *str)
 	char	*word;
 
 	i = 0;
-	while (!ft_iswhitespace(str[i]))
+	while (!ft_iswhitespace(str[i]) && str[i] != '$' && str[i])
+	{
+		ft_printf("str[i] = %c\n", str[i]);
 		i++;
+	}
 	i--;
 	word = malloc(sizeof(char) * i);
-	j = -1;
-	while (str[++j] && j <= i)
+	j = 0;
+	while (str[j] && j <= i)
+	{
 		word[j] = str[j];
-	word[++j] = '\0';
+		j++;
+	}
+	word[j] = '\0';
 	return (word);
 }
 
@@ -48,8 +54,35 @@ static char	*store_special(char *str)
 	j = -1;
 	while (str[++j] && j <= i)
 		word[j] = str[j];
-	word[++j] = '\0';
+	word[j] = '\0';
 	return (word);
+}
+
+
+static char	*process_var(int *i, char *str, char *temp, t_env *env)
+{
+		char	*word;
+
+		ft_printf("process temp = %s\n", temp);
+		if (str[*i + 1] == '?') 
+		{
+			ft_printf("exit status\n");
+			(*i)++;
+		}
+		else if (ft_isalpha(str[*i + 1]))
+		{
+			word = store_word(str + *i + 1); //saves into 'word' whatever comes after '$'
+			*i += ft_strlen(word);
+			word = get_env_var(env, word); //searches for a env called 'word' and then replaces it with its value
+			if (ft_strlen(word) > 0)
+				temp = ft_strjoin(temp, word);
+				
+		}
+		else if (ft_isdigit(str[*i + 1]))
+			ft_printf("\n\nsyntax_error_number\n\n"); //syntax_error('$');
+		else
+			ft_printf("\n\nsyntax_error\n\n");
+		return (temp);
 }
 
 char	*var_to_value(char *str, t_env *env)
@@ -64,32 +97,14 @@ char	*var_to_value(char *str, t_env *env)
 	{
 		
 		if (str[i] == '$')
-		{
-			if (str[i + 1] == '?') 
-				ft_printf("exit status\n");
-
-			else if (ft_isdigit(str[i + 1]))
-				ft_printf("\n\nsyntax_error_number\n\n"); //syntax_error('$');
-
-			else if (ft_isalpha(str[i + 1]))
-			{
-				word = store_word(str + i + 1); //saves into 'word' whatever comes after '$'
-				i += ft_strlen(word);
-				word = get_env_var(env, word); //searches for a env called 'word' and then replaces it with its value
-				if (word)
-					temp = ft_strjoin(temp, word);
-			}
-			else
-			{
-				ft_printf("\n\nsyntax_error\n\n");
-				return (NULL);
-			}
-		}
+			temp = process_var(&i, str, temp, env);
 		else if (str[i] == '"' && str[i + 1] != '"')
 		{
+			//check end quote
 			word = store_special(str + i + 1); //saves into 'word' whatever comes after '$'
 			i += ft_strlen(word);
 			temp = ft_strjoin(temp, word);
+			ft_printf("word is = %s\n", word);
 		}
 		else
 			temp = ft_strjoin(temp, ft_substr(str, i, 1));
