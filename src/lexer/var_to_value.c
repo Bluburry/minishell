227f310fd9af20fda865a69c92ce2137dedf4570
Andre Miguel De Barros Moura <ade-barr@c1r8s13.42porto.com>
2023-10-31 13:03:52 +0000

    wip

--- src/lexer/var_to_value.c
+++ src/lexer/var_to_value.c
@@ -6,7 +6,7 @@
 /*   By: ade-barr <ade-barr@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 10:14:01 by ade-barr          #+#    #+#             */
-/*   Updated: 2023/10/30 01:29:14 by ade-barr         ###   ########.fr       */
+/*   Updated: 2023/10/31 12:58:31 by ade-barr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,22 @@
 //int i ---------- index counter to malloc correct size
 //int j ---------- index that starts from 0 again, to copy 'str' into 'word' up to 'i' size
 //char *word ----- string that stores anything after the '$' untill it finds a space (doesnt copy the space)
+static int	find_end(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (!ft_iswhitespace(str[i]) && str[i] != '$'
+			&& str[i] != '|' && str[i] != '"'
+			&& str[i] != '\'' && str[i])
+		i++;
+	if (str[i] == '$' || ft_iswhitespace(str[i])
+			|| str[i] == '|' || str[i] == '"'
+			|| str[i] == '\'')
+			i--;
+	return (i);
+}
+
 static char	*store_word(char *str)
 {
 	int	i;
@@ -23,13 +39,8 @@ static char	*store_word(char *str)
 	char	*word;
 
 	i = 0;
-	while (!ft_iswhitespace(str[i]) && str[i] != '$' && str[i])
-	{
-		ft_printf("str[i] = %c\n", str[i]);
-		i++;
-	}
-	i--;
-	word = malloc(sizeof(char) * i);
+	word = malloc(sizeof(char) * i + 1);
+	i = find_end(str);
 	j = 0;
 	while (str[j] && j <= i)
 	{
@@ -40,30 +51,12 @@ static char	*store_word(char *str)
 	return (word);
 }
 
-static char	*store_special(char *str)
-{
-	int	i;
-	int	j;
-	char	*word;
-
-	i = 0;
-	while (str[i] != '"')
-		i++;
-	i--;
-	word = malloc(sizeof(char) * i);
-	j = -1;
-	while (str[++j] && j <= i)
-		word[j] = str[j];
-	word[j] = '\0';
-	return (word);
-}
-
-
 static char	*process_var(int *i, char *str, char *temp, t_env *env)
 {
 		char	*word;
+		char	*ret_str;
 
-		ft_printf("process temp = %s\n", temp);
+		ret_str = ft_calloc(sizeof(char), 1);
 		if (str[*i + 1] == '?') 
 		{
 			ft_printf("exit status\n");
@@ -73,41 +66,54 @@ static char	*process_var(int *i, char *str, char *temp, t_env *env)
 		{
 			word = store_word(str + *i + 1); //saves into 'word' whatever comes after '$'
 			*i += ft_strlen(word);
+			ft_printf("'%s'\n", word);
 			word = get_env_var(env, word); //searches for a env called 'word' and then replaces it with its value
-			if (ft_strlen(word) > 0)
-				temp = ft_strjoin(temp, word);
-				
+			ft_printf("'%s'\n", word);
+			if (word)
+			{
+				ret_str = ft_strjoin(temp, word);
+				return (ret_str);
+			}
+			ret_str = ft_strjoin(temp, "");
 		}
 		else if (ft_isdigit(str[*i + 1]))
 			ft_printf("\n\nsyntax_error_number\n\n"); //syntax_error('$');
 		else
-			ft_printf("\n\nsyntax_error\n\n");
-		return (temp);
+		{
+			ft_printf("syntax_error\n");
+			return (temp);
+		}
+		if (ret_str)
+			return (ret_str);
+		return (ret_str);
 }
 
 char	*var_to_value(char *str, t_env *env)
 {
 	int	i;
-	char	*word;
 	char	*temp;
 
 	i = -1;
 	temp = ft_calloc(sizeof(char), 1);
 	while (str[++i])
 	{
-		
 		if (str[i] == '$')
-			temp = process_var(&i, str, temp, env);
+		{
+			if (ft_iswhitespace(str[i + 1]) || !str[i + 1])
+				temp = ft_strjoin(temp, "$");
+			else
+				temp = process_var(&i, str, temp, env);
+		}
 		else if (str[i] == '"' && str[i + 1] != '"')
 		{
-			//check end quote
-			word = store_special(str + i + 1); //saves into 'word' whatever comes after '$'
-			i += ft_strlen(word);
-			temp = ft_strjoin(temp, word);
-			ft_printf("word is = %s\n", word);
+			i++;
+			temp = process_var(&i, str, temp, env);
 		}
-		else
+		else if (str[i])
+		{
 			temp = ft_strjoin(temp, ft_substr(str, i, 1));
+		}
 	}
+	ft_printf("temp before out = '%s'\n", temp);
 	return (temp);
 }
