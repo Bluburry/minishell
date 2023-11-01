8706c20a065fcbcf94bd9d91c730e2e7f5c7b101
Sheepcoat <ambmoura@live.com.pt>
2023-11-01 18:53:39 +0000

    var_to_value wip

--- src/lexer/var_to_value.c
+++ src/lexer/var_to_value.c
@@ -12,10 +12,6 @@
 
 #include "minishell.h"
 
-//mallocs and stores any valid input after '$' [QUESTION]: where do i free this? To be continued... 
-//int i ---------- index counter to malloc correct size
-//int j ---------- index that starts from 0 again, to copy 'str' into 'word' up to 'i' size
-//char *word ----- string that stores anything after the '$' untill it finds a space (doesnt copy the space)
 static int	find_end(char *str)
 {
 	int	i;
@@ -23,12 +19,17 @@ static int	find_end(char *str)
 	i = 0;
 	while (!ft_iswhitespace(str[i]) && str[i] != '$'
 			&& str[i] != '|' && str[i] != '"'
+			&& str[i] != '?' && str[i] != ';'
+			&& str[i] != '<' && str[i] != '>'
 			&& str[i] != '\'' && str[i])
 		i++;
 	if (str[i] == '$' || ft_iswhitespace(str[i])
-			|| str[i] == '|' || str[i] == '"'
-			|| str[i] == '\'')
-			i--;
+			|| str[i] != '|' || str[i] != '"'
+			|| str[i] != '?' || str[i] != ';'
+			|| str[i] != '<' || str[i] != '>'
+			|| str[i] != '\'')
+		i--;
+	ft_printf("i is %d after count \n", i);
 	return (i);
 }
 
@@ -39,8 +40,8 @@ static char	*store_word(char *str)
 	char	*word;
 
 	i = 0;
-	word = malloc(sizeof(char) * i + 1);
 	i = find_end(str);
+	word = malloc(sizeof(char) * i + 2);
 	j = 0;
 	while (str[j] && j <= i)
 	{
@@ -57,18 +58,16 @@ static char	*process_var(int *i, char *str, char *temp, t_env *env)
 		char	*ret_str;
 
 		ret_str = ft_calloc(sizeof(char), 1);
-		if (str[*i + 1] == '?') 
+		if (str[*i + 1] && str[*i + 1] == '?') 
 		{
 			ft_printf("exit status\n");
 			(*i)++;
 		}
-		else if (ft_isalpha(str[*i + 1]))
+		else if (str[*i + 1] && ft_isalpha(str[*i + 1]))
 		{
 			word = store_word(str + *i + 1); //saves into 'word' whatever comes after '$'
 			*i += ft_strlen(word);
-			ft_printf("'%s'\n", word);
 			word = get_env_var(env, word); //searches for a env called 'word' and then replaces it with its value
-			ft_printf("'%s'\n", word);
 			if (word)
 			{
 				ret_str = ft_strjoin(temp, word);
@@ -76,7 +75,7 @@ static char	*process_var(int *i, char *str, char *temp, t_env *env)
 			}
 			ret_str = ft_strjoin(temp, "");
 		}
-		else if (ft_isdigit(str[*i + 1]))
+		else if (str[*i + 1] && ft_isdigit(str[*i + 1]))
 			ft_printf("\n\nsyntax_error_number\n\n"); //syntax_error('$');
 		else
 		{
@@ -93,11 +92,12 @@ char	*var_to_value(char *str, t_env *env)
 	int	i;
 	char	*temp;
 
-	i = -1;
+	i = 0;
+	//ft_printf("\n........................\nvar_to_value input str is = '%s'\n", str);
 	temp = ft_calloc(sizeof(char), 1);
-	while (str[++i])
+	while (str[i])
 	{
-		if (str[i] == '$')
+		if (str[i] && str[i] == '$')
 		{
 			if (ft_iswhitespace(str[i + 1]) || !str[i + 1])
 				temp = ft_strjoin(temp, "$");
@@ -107,13 +107,15 @@ char	*var_to_value(char *str, t_env *env)
 		else if (str[i] == '"' && str[i + 1] != '"')
 		{
 			i++;
-			temp = process_var(&i, str, temp, env);
+			if (str[i] && str[i] == '$')
+				temp = process_var(&i, str, temp, env);
+			else
+				temp = ft_strjoin(temp, ft_substr(str, i, 1));
 		}
 		else if (str[i])
-		{
 			temp = ft_strjoin(temp, ft_substr(str, i, 1));
-		}
+		i++;
 	}
-	ft_printf("temp before out = '%s'\n", temp);
+	//ft_printf("string after var_to_value = '%s'\n", temp);
 	return (temp);
 }
