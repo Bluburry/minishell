5aed32fe3b1a29e23e829c02493db4edac04531e
Renato Marques Neto <remarque@student.42porto.com>
2024-01-04 13:33:44 +0000

    heredoc not working with pipes

--- src/redirect/redirection.c
+++ src/redirect/redirection.c
@@ -1,3 +1,4 @@
+#include "libft.h"
 #include "minishell.h"
 
 int	redir_appd(char *path)
@@ -45,31 +46,40 @@ int	redir_in(char *path)
 	return (1);
 }
 
+#define PTH "/tmp/MS_HEREDOC"
+#define ERR_STR "Warning: heredoc delimited by end of file instead of %s\n"
+
 int	redir_heredoc(char *stop, t_data *d)
 {
-	int		fl;
-	char	*str;
-
-	fl = open("/tmp/MS_HEREDOC", O_WRONLY | O_TRUNC | O_CREAT, 0666);
+	auto int fl = open(PTH, O_WRONLY | O_TRUNC | O_CREAT, 0666);
+	auto char *str = NULL;
 	if (fl == -1)
 		return (printf("Error opening temp file.\n"), 0);
 	dup2(d->stdin, STDIN_FILENO);
 	while (g_sig != SIGINT)
 	{
+		//printf("∙ ");
+		//str = ft_gnl_reset(fl, false);
+		//auto char *temp = ft_strrchr(str, '\n');
+		//if (temp != NULL)
+		//	*temp = '\0';
 		str = readline("∙ ");
-		if (str == NULL || !ft_strcmp(str, stop))
+		if (str == NULL)
+		{
+			printf(ERR_STR, stop);
+			break ;
+		}
+		else if (!ft_strcmp(str, stop))
 			break ;
 		ft_putstr_fd(str, fl);
 		ft_putstr_fd("\n", fl);
 		free(str);
 	}
 	free(str);
+	//ft_gnl_reset(fl, true);
 	close(fl);
-	fl = open("/tmp/MS_HEREDOC", O_RDONLY, 0);
+	fl = open(PTH, O_RDONLY, 0);
 	if (fl == -1)
 		return (printf("Error opening temp file.\n"), 0);
-	unlink("/tmp/MS_HEREDOC");
-	dup2(fl, STDIN_FILENO);
-	close(fl);
-	return (1);
+	return (unlink(PTH), dup2(fl, STDIN_FILENO), close(fl), 1);
 }
